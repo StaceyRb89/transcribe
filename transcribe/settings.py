c781c76ac2c5a25534a46dbb08a58b79f5be4fb4
Your Name <you@example.com>
2024-01-31 19:13:26 +0000

    fault finding

--- transcribe/settings.py
+++ transcribe/settings.py
@@ -31,7 +31,7 @@ if os.path.isfile('env.py'):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
