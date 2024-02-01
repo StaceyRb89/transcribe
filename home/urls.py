4fc1b8f94dbec2e9c794416ecf69ab51cc9c150a
Your Name <you@example.com>
2024-02-01 21:19:49 +0000

    modals, and faq added

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Index
+from .views import Index, team, faq
 from . import views
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('contact/', views.contact_view, name='contact'),
+    path('team/', team, name='team'),
+    path('faq/', faq, name='faq'),
 ]
