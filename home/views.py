bd2f6840a96a1ba79b15700808eb13ef8cc3de96
Stacey robson <stacey.robson.sr@gmail.com>
2024-02-13 12:59:34 +0000

    team and faq code added

--- home/views.py
+++ home/views.py
@@ -8,3 +8,24 @@ class Index(TemplateView):
 def contact_view(request):
     # Your contact view implementation
     return render(request, 'contact/contact.html') 
+
+#added
+
+# the team
+# views.py
+def team(request):
+    # Your logic to fetch team information goes here
+    team_info = {
+        'team_members': [
+            {'name': 'Member 1', 'position': 'Position 1'},
+            {'name': 'Member 2', 'position': 'Position 2'},
+            # Add more team members as needed
+        ]
+    }
+
+
+    return render(request, 'home/team.html', {'team_info': team_info})
+
+
+def faq (request):
+    return render(request, 'home/faq.html')
